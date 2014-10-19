25408098cb0b1a054bdfab1830661bbce3f16dbf
Francisco Temudo <fthemudo@gmail.com>
2014-10-19 20:09:44 +0100

    Controller specs

---
+++ spec/controllers/jetly_urls_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe JetlyUrlsController do
+  describe '.index' do
+
+    before do
+      get :index
+    end
+
+    it 'should instanciate a jetly_url' do
+      expect(assigns(:jetly_url)).to be_a_new(JetlyUrl)
+    end
+
+    it 'should render jetly index' do
+      expect(response).to render_template(:index)
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+  describe '.create' do
+
+    after do
+      expect(response).to render_template(:index)
+    end
+
+    context 'When shortening a valid url' do
+      let(:url_to_shorten) { 'http://www.sapo.pt' }
+      let(:shortened_jetly_url) {
+        mock_model 'JetlyUrl', complete_url: url_to_shorten, url_hash: 'http://localhost:3000/shrt', visits: 0
+      }
+
+      it 'should fetch a shortened url' do
+        allow(JetlyUrl).to receive(:shorten).with(url_to_shorten).and_return(shortened_jetly_url)
+        post :create, jetly_url: { complete_url: url_to_shorten }
+        expect(assigns(:jetly_url)).to be(shortened_jetly_url)
+      end
+    end
+
+  end
+end
