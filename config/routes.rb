4554483638f884a072d6567a1184fa75ba3e6d9f
Francisco Temudo <fthemudo@gmail.com>
2014-10-20 11:16:53 +0100

    All creation requests are posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :jetly_urls, path: '/'
+  resources :jetly_urls, path: '/', only: [:index, :create]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
